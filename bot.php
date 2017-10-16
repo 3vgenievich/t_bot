0e2549a1c7bab73e48f89d958f817afebc3ca2ba
3vgenievich <balandus@bk.ru>
2017-10-16 15:21:35 +0500

    t_bot1

--- bot.php
+++
@@ -1,3 +0,0 @@
-<?php
-$output=json_decode(file_get_contents('php;//input'),true);
-file_put_contents("logs.txt",$output);
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+$output=json_decode(file_get_contents('php;//input'),true);
+file_put_contents("logs.txt",$output);
