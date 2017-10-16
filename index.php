e495ca4ef760441f15f2e20aef8ea330c68de738
3vgenievich <balandus@bk.ru>
2017-10-16 15:56:05 +0500

    t_bot

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI';
-sendMessage($token,$id);
 $message='qweqwe';
+sendMessage($token,$id,$message);
 function sendMessage($token, $id,$message)
 {
 file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=".$message);
