2b77fdcddcebd0012393a44b1abd0f1c02842f53
3vgenievich <balandus@bk.ru>
2017-10-16 16:05:34 +0500

    t_bot

--- index.php
+++ index.php
@@ -4,6 +4,20 @@ $id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI';
 $message= $output['message']['text'];
 
+switch ($message){
+
+    case 'привет':
+        $message='Привет';
+        sendMessage($token,$id,$message);
+        break;
+    case 'Как дела?':
+        $message='ХУЙОВА';
+        sendMessage($token,$id,$message);
+        break;
+    default:
+        $message='что?';
+        sendMessage($token,$id,$message);
+}
 sendMessage($token,$id,$message);
 function sendMessage($token, $id,$message)
 {
