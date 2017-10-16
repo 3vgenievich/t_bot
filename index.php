0bd2f14a4c7526a5c534577dab3ed55807321661
Maxim Balandin <balandus@bk.ru>
2017-10-16 15:26:23 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-$output=json_decode(file_get_contents('php;//input'),true);
+$output = json_decode(file_get_contents('php;//input'),true);
+$id = $output['message']['text'];
 file_put_contents("logs.txt",$output);
