ab5ebef8b5d575fea1b6975c0cf1a2ee22be7718
Maxim Balandin <balandus@bk.ru>
2017-10-16 15:34:53 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 $output = json_decode(file_get_contents('php://input'),true);
-$id = $output['message']['text'];
+$id = $output['message']['chat']['id'];
+file_get_contents("https://api.telegram.org/bot469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI/sendMessage?chat_id=".$id."&text=hello")
 file_put_contents("logs.txt",$id);
