48ca2cb727b839a02e3699b67282a69f76d2a862
Maxim Balandin <balandus@bk.ru>
2017-11-02 11:10:01 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI';
 $message= $output['message']['text'];
-
+$location = new SendLocation();
 switch ($message){
 
     case '/start':
         $message='Привет';
-        sendMessage($token,$id,$message.KeyboardMenu());
+        sendMessage($token,$id,,$message.KeyboardMenu());
         break;
     case 'Где я?':
         $message='Ваше местоположение:';
-        sendMessage($token,$id,$message.KeyboardMenu());
+        sendMessage($token,$id,$location,$message.KeyboardMenu());
         break;
     case 'как дела?':
         $message='отлично';
@@ -30,14 +30,11 @@ file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_i
 file_put_contents("logs.txt",$id);
 //тест клавиатуры
 function KeyboardMenu(){
-    //$buttons = [['Где я?','request_location'=>true],['how are you'],['two'],['three']];
-    $keyboard =json_encode($keyboard=['keyboard' => [[['Где я?','request_location'=>true,
-                                                    ['how are you'],
-                                                    ['two'],
-                                                    ['three']]],
+    $buttons = [['Где я?','request_location'=>true],['how are you'],['two'],['three']];
+    $keyboard =json_encode($keyboard=['keyboard' =>$buttons,
                                         'resize_keyboard' => true,
                                         'one_time_keyboard'=> false,
                                         'selective' => true,
-                                      //  'request_location'=> true]);
+                                        'request_location'=> true]);
     $reply_markup ='&reply_markup='.$keyboard.'';
     return $reply_markup;
