6ed0e9d6023cc638ae8588e1bf800c24090e0102
3vgenievich <balandus@bk.ru>
2017-11-11 17:01:48 +0500

    t_bot

--- index.php
+++ index.php
@@ -2,19 +2,15 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI';
-$message= $output['message']['text'];
-$lat=$output['latitude']; //Широта
-$lon=$output['longitude'];  //Долгота
-function send_location($id, $lat, $lon, $keyboard = null, $token){
+//$message= $output['message']['text'];
+$message = isset($output->message) ? $output->message : null;
+
+function send_location($id, $lat, $lon, $keyboard = null, $token)
+{
     $data = array();
     $data["chat_id"] = $id;
-    $data["latitude"] = $lat;
-    $data["longitude"] = $lon;
-
-    if(isset($keyboard))
-        $data["reply_markup"] = $keyboard;
-
-    send("sendLocation", $data, $token);
+    $data["latitude"] = $lat;//широта
+    $data["longitude"] = $lon;//долгота
 }
 switch ($message){
 
@@ -22,20 +18,19 @@ switch ($message){
         $message='Привет!';
         sendMessage($token,$id,$message.KeyboardMenu());
         break;
-    case 'Где я?':
-        $message='Вы находитесь здесь: ';
+    case 'Моё местоположение':
+        if (isset($message->location)) {
+            $message = '"Вы находитесь здесь:" ,$lat,$lon';
+        }
+        else {
+            $msg = "Нажмите кнопку \"Отправить мое местоположение\"";
+        }
         sendMessage($token,$id,$message.KeyboardMenu());
         break;
     case 'Справка':
         $message='по вопросам разработки : vk.com/3vgenievich';
         sendMessage($token,$id,$message.KeyboardMenu());
         break;
-    case 'location':
-        if (isset($message->location)) {
-            $message = "Отлично. Ваш заказ принят.";
-            sendMessage($token,$id,$message.KeyboardMenu());
-        }
-            break;
     default:
         $message='Неправильный запрос.';
         sendMessage($token,$id,$message);
@@ -48,7 +43,7 @@ file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_i
 file_put_contents("logs.txt",$id);
 //тест клавиатуры
 function KeyboardMenu(){
-    $buttons = [[['text'=>"Где я?", 'request_location'=>true]],[['text'=>"Показать автосервисы"]],[['text'=>"Справка"]]];
+    $buttons = [[['text'=>"Моё местоположение", 'request_location'=>true]],[['text'=>"Показать автосервисы"]],[['text'=>"Справка"]]];
     $keyboard =json_encode($keyboard=['keyboard' => $buttons,
                                         'resize_keyboard' => true,
                                         'one_time_keyboard'=> false,
