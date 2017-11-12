2c58701fe4739ddffa8ed3e04c076f9c611a1be6
3vgenievich <balandus@bk.ru>
2017-11-12 20:02:26 +0500

    t_bot

--- index.php
+++ index.php
@@ -69,6 +69,6 @@ function is_empty(&$var)
  */
 function get_address($lat, $lon, $ApiKey)
 {
-    file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=$ApiKey"); //гугл апи. возвращает адрес по координатам
-    return json_decode(file_get_contents('php://input'),true);
+    $uri="https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=$ApiKey"; //гугл апи. возвращает адрес по координатам
+    return json_decode(file_get_contents($uri));
 }
