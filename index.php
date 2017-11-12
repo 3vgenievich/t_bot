d41eb770570294a98d597b37fed7f1c5cef34ebd
3vgenievich <balandus@bk.ru>
2017-11-13 00:53:52 +0500

    t_bot

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 $output = json_decode(file_get_contents('php://input'),true);
-$id = $output["message"]["chat"]["id"];
+$id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI'; //Токен телеграм. Убрать в отдельный файл!!!
 $ApiKey='AIzaSyDJy5MnyWi09N_HXiPBuDHyC2ZhIe9kZf4';      //ApiKey гугл .
-$message= $output["message"]["text"];
-$Location=$output["chat_id"]["longitude"]["latitude"];
+$message= $output['message']['text'];
+$Location=$output['chat_id']['longitude']['latitude'];
 switch ($message) {
     case '/start':
-        $message = 'Привет! Нажми отпраить местоположение чтобы начать.';
+        $message = 'Привет! Нажми отправить местоположение чтобы начать.';
         sendMessage($token, $id, $message . KeyboardMenu());
         break;
     case $Location:
-        $response = json_decode($Location, true);
-        $lat = $response[0]["latitude"];
-        $lon = $response[0]["longitude"];
+        $response = json_decode(file($Location), true);
+        $lat = $response[0]['latitude'];
+        $lon = $response[0]['longitude'];
         if (isset($lat))
             {
                 $message = "Отлично! ваше местонахождение определено." . $lat . $lon;
