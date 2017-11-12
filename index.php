b1ae1f8d74136c3fe511df524356c7865c799f1a
3vgenievich <balandus@bk.ru>
2017-11-13 01:31:15 +0500

    t_bot

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ $id = $output['message']['chat']['id'];
 $token='469123782:AAHOpN4Fqow0wNjPYTW3wIke37V5JTwp9iI'; //Токен телеграм. Убрать в отдельный файл!!!
 $ApiKey='AIzaSyDJy5MnyWi09N_HXiPBuDHyC2ZhIe9kZf4';      //ApiKey гугл .
 $message= $output['message']['text'];
-$Location=$output['location']['longitude']['latitude'];
+$Location=$output['longitude']['latitude'];
+$response = json_decode(($Location), true);
 switch ($message) {
     case '/start':
         $message = 'Привет! Нажми отправить местоположение чтобы начать.';
         sendMessage($token, $id, $message . KeyboardMenu());
         break;
-    case $Location:
-        $response = json_decode(($Location), true);
+    case $response:
         $lat = $response['latitude'];
         $lon = $response['longitude'];
         if (isset($lat))
