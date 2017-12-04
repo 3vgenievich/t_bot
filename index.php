af1c08584494d6721b2389259ff6eb08109bc8c3
3vgenievich <balandus@bk.ru>
2017-12-04 15:13:33 +0500

    t_bot

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ switch ($message) {
     case $Location['location']:
         $lat = $Location['latitude'];
         $lon = $Location['longitude'];
-        if (isset($lat,$lon))
-            {
+       # if (isset($lat,$lon))
+        #    {
                 $message = "Отлично! ваше местонахождение определено." .$lat.$lon;
-            }
-        else
-            {
-                $message ="error";
-            }
+         #   }
+        #else
+         #   {
+          #      $message ="error";
+           # }
         sendMessage($token, $id, $message,$lat,$lon . KeyboardMenu() . get_address($lat, $lon, $ApiKey));
         break;
     case 'Показать автосервисы':
