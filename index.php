6a185925dab66a9d724969b90585b89fc44e819a
Maxim Balandin <balandus@bk.ru>
2017-12-04 09:18:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ switch ($message) {
         $message = 'Привет! Нажми отправить местоположение чтобы начать.';
         sendMessage($token, $id, $message . KeyboardMenu());
         break;
-    case $Location:
+    case $Location['location']:
         $lat = $Location['location'];
         $lon = $Location['longitude'];
         if (isset($Lat))
