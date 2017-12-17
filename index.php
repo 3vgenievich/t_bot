3a43dc8af1f3c7f984e2bc1e138b7be2c98b9106
3vgenievich <balandus@bk.ru>
2017-12-17 17:41:15 +0500

    t_bot

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ switch ($message) {
         $lat = $Location['latitude'];
         $lon = $Location['longitude'];
 
-        $conn->query("UPDATE heroku_b8eb8cf712bc20c.locations set id ='{$id['id']}',lat='{$lat}',lon='$lon'");
+        $conn->query("INSERT INTO heroku_b8eb8cf712bc20c.locations(id,lat,lon) VALUES ('{$id['id']}',lat='{$lat}',lon='$lon')");
         if (isset($lat,$lon))
             {
                 $message = "Отлично! ваше местонахождение определено.  Широта: ".$lat."  Долгота: ".$lon."  Адрес: ".get_address($lat,$lon,$ApiKey);
