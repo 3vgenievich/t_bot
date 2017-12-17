421cf12dcb20988efc1c9034b8d5ac4f5b463b67
3vgenievich <balandus@bk.ru>
2017-12-17 18:18:08 +0500

    t_bot

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <?php
-
-/* ------------------------------------------------------------------------
- * to do:
- * +++1)Токен телеграм. Убрать в отдельный файл!
- * +++2)ApiKey гугл . Убрать в отдельный файл!
- * +++3)Создать вторую клавиатуру!!!
- * +++4)Починить get_address
- * 5)Разобраться с $lat $lon - добавить бд для широты и долготы, загрузка широты и долготы из бд
- * 6)Написать логику поиска ближайших сервисов/шиномонтажек
- * 7)Написать импорт номеров автоэвакуаторов из БД
- * 8)Изменить расширение файлов с токеном и apikey . закрыть к ним доступ на сервере.
- * 9
- *-------------------------------------------------------------------------
- * */
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token=file_get_contents('./token.txt');
 $ApiKey=file_get_contents('./ApiKey.txt');
 $message= $output['message']['text'];
 $Location=$output['message']['location'];
-/*
-###ПОДКЛЮЧЕНИЕ К  БД###
+
 $url = parse_url(getenv("mysql://bf201afc3c04bc:67a8b83e@eu-cdbr-west-01.cleardb.com/heroku_b8eb8cf712bc20c?reconnect=true"));
 $server ='eu-cdbr-west-01.cleardb.com';
 $username = 'bf201afc3c04bc';
 $password = '67a8b83e';
 $db = 'heroku_b8eb8cf712bc20c';
 $conn = new mysqli($server, $username, $password, $db);
-###*/
+
 switch ($message){
-    /*клавиатура 1*/
     case '/start':
         $message = 'Привет! Нажми отправить местоположение чтобы начать.';
         sendMessage($token, $id, $message . KeyboardMenu());
@@ -37,7 +21,7 @@ switch ($message){
     case $Location['location']:
         $lat = $Location['latitude'];
         $lon = $Location['longitude'];
-        /*$conn->query("UPDATE heroku_b8eb8cf712bc20c.locations SET id=".$id['id'].",lat=".$lat.",lon=".$lon);*/
+        $conn->query("UPDATE heroku_b8eb8cf712bc20c.locations SET id=".$id['id'].",lat=".$lat.",lon=".$lon);
         if (isset($lat,$lon))
             {
                 $message = "Отлично! ваше местонахождение определено.  Широта: ".$lat."  Долгота: ".$lon."  Адрес: ".get_address($lat,$lon,$ApiKey);
@@ -94,7 +78,7 @@ switch ($message){
         $message='Неправильный запрос. Для получения справки нажмите "Справка"';
         sendMessage($token,$id,$message.KeyboardMenu());
 }
-///sendMessage($token,$id,$message);
+
 function sendMessage($token, $id,$message)
 {
 file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=".$message);
@@ -134,7 +118,7 @@ function get_nearest_places($lat,$lon,$keyword,$ApiKey)
 {
     $url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$lat.",".$lon."&radius=5000&type=car_repair&keyword=".$keyword."&key=".$ApiKey."&language=ru";//находит автосервисы в радиусе 5км
     $place = get_object_vars(json_decode(file_get_contents($url)));
-    $place = /*$place['results'][1]->name.",".$place['results'][1]->opening_hours.",".*/$place['results'][1]->vicinity;
+    $place = $place['results'][1]->name.",".$place['results'][1]->opening_hours.",".$place['results'][1]->vicinity;
     return $place;
 
     /*
